fefca7c5872b78bec6f447737c493ddb96cc5a3d
StopChineseOrganTrafficking <ungoal16@gmail.com>
2017-03-29 10:37:56 -0400

    update 10:37 3/29/17
    made this work
    changed the content of the javascript tabs

--- interact.js
+++ interact.js
@@ -6,23 +6,19 @@ $(document).ready(function() {
         $(this).css('color', 'black');
     });
     $('#info').click(function() {
-        $('p').html('This website is being hosted using <a href="https://github.com/" target="_blank">GitHub</a>. This project is being developed just for fun. <br> Subscribe to <a href="https://www.youtube.com/channel/UCWVgMO59hA3vW4OG_p9H6Dg" target="_blank">my YouTube channel</a> for video updates on this project.');
+        $('#content').html('<p>Organ Trafficking in China is a multi -million dollar busniness. Although it might generate lots of money for Chins, it is exremely creul and harmful to everyone.</p>');
+        updates on this project.');
         $('#tab_h2').text('Info');
-        $('title').text('1 - Info') 
-    });      
-    $('#links').click(function() {
-        $('p').html('Here are some websites that help me out: <br> <br> <a href="https://github.com/" target="_blank">GitHub</a> <br> <br> <a href="https://www.codecademy.com/learn" target="_blank">codecademy</a> <br> <br> <a href="https://www.w3schools.com/" target="_blank">w3shcools</a> <br> <br> Press collaborate if you want to help out!');
-        $('#tab_h2').text('Links');
-        $('title').text('1 - Links');
+        $('title').text('Info'); 
     });
-    $('#collab').click(function() {
-        $('p').html('Follow me on <a href="https://github.com/KazanPickle" target="_blank">my GitHub profile</a> if you want to help me with this project.');
-        $('#tab_h2').html('Collaborate');
-        $('title').text('1 - Collab');
+    $('#contact').click(function() {
+        $('#content').html('<p>Email : ungoal16@gmail.com</p>');
+        $('#tab_h2').html('Contact Us');
+        $('title').text('Contact Us');
     });
-    $('#YT').click(function() {
-        $('p').html('A video will appear here as soon as one is uploaded.')
-        $('#tab_h2').html('YouTube');
-        $('title').text('1 - YouTube');
+    $('#sources').click(function() {
+        $('#content').html('<p>This is currently being assembled</p>');
+        $('#tab_h2').html('Sources');
+        $('title').text('Sources');
     });
 });
