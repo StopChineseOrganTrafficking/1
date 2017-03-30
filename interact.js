d275b0d4379b5b892292ec4eb4b1f2ca5b98270d
StopChineseOrganTrafficking <ungoal16@gmail.com>
2017-03-30 10:37:57 -0400

    update 10:37 3/30/17
    changed javascript to include new info

--- interact.js
+++ interact.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
         $(this).css('color', 'black');
     });
     $('#info').click(function() {
-        $('#content').html('<p>Organ Trafficking in China is a multi -million dollar busniness. Although it might generate lots of money for Chins, it is exremely creul and harmful to everyone.</p>');
+        $('#content').html('<p>Organ Trafficking in China is a multi -million dollar busniness. Although it might generate lots of money for China, it is exremely creul and harmful to everyone. Every year, China illegaly carries out over 60,000 organ transplants. Most of these come form people who dont agree with China's political stance, or religon. These people are killed based on their belifs and executed by the government and used for illegal organ transplants.</p>
+                        <p>China is showing no signs of stopping this creul practice on innocent people. They have made up fasle claims stateing that they dont use this practice anymore. But many people dissagree with this claim.</p>');
         $('#tab_h2').text('Info');
         $('title').text('Info'); 
     });
