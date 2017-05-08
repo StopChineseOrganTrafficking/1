1f57465259921c435c276756514eae6d2cf385e7
KazanPickle <bryanchristopherjohnson@gmail.com>
2017-05-08 19:24:17 -0400

    Incorrect syntax hotfix

--- interact.js
+++ interact.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#header').click(function() {
-        $('.top').html('<ul><li><a href="#info">Information</a></li><li><a href="#evidence">Evidence</a></li><li><a href="source">Source Of Organs</a></li><li><a href="#help">Support Us</a></li></ul>');
+        $('.top').html('<ul><li><a href="#info">Information</a></li><li><a href="#evidence">Evidence</a></li><li><a href="#source">Source Of Organs</a></li><li><a href="#help">Support Us</a></li></ul>');
     });
     $('#header').mouseleave(function() {
       $('.top').html('<h1>Stop Chinese Organ Trafficking</h1>');
