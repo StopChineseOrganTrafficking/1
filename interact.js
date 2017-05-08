d2e3dbb688697e80f4d65c8cd9e00f6d84d9439c
KazanPickle <bryanchristopherjohnson@gmail.com>
2017-05-08 19:23:06 -0400

    Javascript partially complete
    The header now transforms in to a navigation bar when clicked and back
    in to a banner when the mouse leaves the header. Functionality to shrink
    the banner to "SCOT" should be added within a few days.

--- interact.js
+++ interact.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-    $('#header').onclick(function() {
-        $('#title').text('wow');
+    $('#header').click(function() {
+        $('.top').html('<ul><li><a href="#info">Information</a></li><li><a href="#evidence">Evidence</a></li><li><a href="source">Source Of Organs</a></li><li><a href="#help">Support Us</a></li></ul>');
+    });
+    $('#header').mouseleave(function() {
+      $('.top').html('<h1>Stop Chinese Organ Trafficking</h1>');
     });
 });
