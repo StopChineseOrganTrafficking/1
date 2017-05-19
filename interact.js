4b0b26217006c0077d7e8ff59413bb54d1726faa
KazanPickle <bryanchristopherjohnson@gmail.com>
2017-05-19 19:26:54 -0400

    Mobile support
    The spacer used to keep the banner at the proper height above the
    content div was edited to make it "invisible", which really means white.
    Also, the ids used for the navigation bar were put in the seperators to
    hopefully make the text readable when the corresponding button is
    pressed.

--- interact.js
+++ interact.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     $('#header').click(function() {
         $('.top').html('<ul><li><a href="#info">Information</a></li><li><a href="#evidence">Evidence</a></li><li><a href="#source">Source Of Organs</a></li><li><a href="#help">Support Us</a></li></ul>');
-    });
-    $('#header').mouseleave(function() {
-      $('.top').html('<h1>Stop Chinese Organ Trafficking</h1>');
+        $('#content').click(function() {
+          $('.top').html('<h1>Stop Chinese Organ Trafficking</h1>');
+        });
+        $('#footer').click(function() {
+          $('.top').html('<h1>Stop Chinese Organ Trafficking</h1>');
+        });
     });
 });
