018ef12074b994720cd34d2384998288cbef4eff
KazanPickle <bryanchristopherjohnson@gmail.com>
2017-05-29 22:28:49 -0400

    hotfix for header
    the previous version of the code made it so that you could not click out
    of the header.

--- interact.js
+++ interact.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
     $('#header').click(function() {
         $('.top').html('<ul id="nav_bar"><li><a href="#info">Information</a></li><li><a href="#evidence">Evidence</a></li><li><a href="#source">Source Of Organs</a></li><li><a href="#help">Support Us</a></li></ul>');
-        $('#content').click(function() {
-          $('.top').html('<h1>Stop Chinese Organ Trafficking</h1>');
-        });
-        $('#footer').click(function() {
+        $('#not-header').click(function() {
           $('.top').html('<h1>Stop Chinese Organ Trafficking</h1>');
         });
     });
